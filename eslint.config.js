7e0616079daa54f7b35731371e9baa275e07e0cc
Wasula Benjamin <wasulabenjamin@gmail.com>
2025-10-16 13:45:51 +0300

    chore(linting): configure prettier, eslint and stylelint rules

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+/*
+ * Wasula Benjamin, Copyright (c) 2025
+ * Contact : wasulabenjamin@gmail.com
+ *
+ * Project Name  : github-templates
+ * File Name     : eslint.config.js
+ * Last Modified : 2025-11-15, 09:49pm
+ */
+
+import vue from "eslint-plugin-vue";
+import prettier from "eslint-plugin-prettier";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsparser from "@typescript-eslint/parser";
+
+export default [
+  {
+    files: ["**/*.{js,ts,vue}"],
+    languageOptions: {
+      parser: tsparser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    plugins: {
+      vue,
+      prettier,
+      "@typescript-eslint": tseslint,
+    },
+    rules: {
+      ...vue.configs["vue3-recommended"].rules,
+      ...tseslint.configs.recommended.rules,
+      "prettier/prettier": "error",
+    },
+  },
+];
